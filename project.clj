5297934ba5aa11f642f93949c6929ea42b45188e
Markus Prinz <markus.prinz@nuclearsquid.com>
2010-05-20 14:18:41 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject booji "0.1.0-SNAPSHOT"
+            :description "Your friendly irc robot and god of buckets"
+            :url "http://cypher.github.com/booji"
+            :namespaces [com.nuclearsquid.booji]
+
+            :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                           [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+                           [irclj "0.3.0-SNAPSHOT"]]
+            )
+
+
